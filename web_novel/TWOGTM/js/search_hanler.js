ee6802ff15d1ed292af3f7cddb39eb95c2443dea
UmU <57792135+LewlShadow@users.noreply.github.com>
2020-08-13 10:07:43 +0700

    update searching and chapter 3,4,5

---
+++ web_novel/TWOGTM/js/search_hanler.js
@@ -0,0 +1,9 @@
+function search(){
+    let x = document.getElementById('search_f').value;
+    window.open("chapter_"+x+".html","_blank");
+}
+
+function search_index(){
+    let x = document.getElementById('search_fin').value;
+    window.open("chapter/chapter_"+x+".html","_blank");
+}
